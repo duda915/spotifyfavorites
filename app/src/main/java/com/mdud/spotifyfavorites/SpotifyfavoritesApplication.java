b94dc086bf9c8465ca99c5756453e5b0363a2dfe
MDud <duda915@gmail.com>
2019-03-06 18:44:05 +0100

    spring initializer

---
+++ app/src/main/java/com/mdud/spotifyfavorites/SpotifyfavoritesApplication.java
@@ -0,0 +1,13 @@
+package com.mdud.spotifyfavorites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyfavoritesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyfavoritesApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/mdud/spotifyfavorites/SpotifyfavoritesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mdud.spotifyfavorites;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpotifyfavoritesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
