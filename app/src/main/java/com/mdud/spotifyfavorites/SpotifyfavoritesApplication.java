4abd247a31b7d6e6593bbe7e0e241bed186a3584
MDud <duda915@gmail.com>
2019-03-06 19:35:22 +0100

    embedded mongo configuration

--- app/src/main/java/com/mdud/spotifyfavorites/SpotifyfavoritesApplication.java
+++ app/src/main/java/com/mdud/spotifyfavorites/SpotifyfavoritesApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpotifyfavoritesApplication {
+public class  SpotifyfavoritesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpotifyfavoritesApplication.class, args);
