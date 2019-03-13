b269a468623edd2aa3699a66a46bcf9df1d8545c
MDud <duda915@gmail.com>
2019-03-13 17:38:53 +0100

    fixed database persisting on windows

--- app/src/main/java/com/mdud/spotifyfavorites/mongo/MongoConfig.java
+++ app/src/main/java/com/mdud/spotifyfavorites/mongo/MongoConfig.java
@@ -1,13 +1,16 @@
 package com.mdud.spotifyfavorites.mongo;
 
+import com.mongodb.MongoClient;
 import de.flapdoodle.embed.mongo.config.IMongodConfig;
 import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.MongoCmdOptionsBuilder;
 import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.config.Storage;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.process.runtime.Network;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.io.File;
 
 import java.io.IOException;
 
@@ -16,11 +19,11 @@ public class MongoConfig {
 
     @Bean
     public IMongodConfig iMongodConfig() throws IOException {
-        String dbDirectory = System.getProperty("user.home") + "/spotifyfavorites";
-        Storage storage = new Storage(dbDirectory, null, 0);
+        Storage storage = new Storage(
+                System.getProperty("user.home") + "/.spotifyfav", null, 0);
 
         return new MongodConfigBuilder()
-                .version(Version.V3_5_5)
+                .version(Version.Main.PRODUCTION)
                 .net(new Net(Network.getFreeServerPort(), Network.localhostIsIPv6()))
                 .replication(storage)
                 .build();
